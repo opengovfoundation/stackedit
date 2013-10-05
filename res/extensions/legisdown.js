1a0e051070715add7532493c10805cea61af90b8
rtsio <rostislav.tsiomenko@gmail.com>
2013-10-05 16:32:04 -0400

    Added whitespace matching and header level

--- res/extensions/legisdown.js
+++ res/extensions/legisdown.js
@@ -3,7 +3,6 @@ define([
 ], function(Extension) {
 
     var legisdown = new Extension("legisdown", "Legisdown");
-
     var eventMgr = undefined;
     legisdown.onEventMgrCreated = function(eventMgrParameter) {
         eventMgr = eventMgrParameter;
@@ -12,25 +11,16 @@ define([
     legisdown.onPagedownConfigure = function(editor) {
         var converter = editor.getConverter();
         converter.hooks.chain("preConversion", function(text) {
-            eventMgr.previewStartTime = new Date();
-            var tmpText = text + "\n\n";
-            var sectionList = [], offset = 0;
-            // Look for titles (excluding gfm blocs)
-
-            tmpText = text.replace(/^\s*(#+)(.*)\{: (\w+='\w+')+\s*\}$/gm, function(match, level, title, attribute) {
+	    var tmpText = text;
+            tmpText = text.replace(/^\s*(#+)(.*)\{: (\w+='\w+')+\s*\}\s*$/gm, function(match, level, title, attribute) {
                 var levelNum = level.length;
                 var title = title.trim();
-                var attributeInfo = attribute.split('=');
-                
-                var returnString = "<h1 data-" + attributeInfo[0] + "=" + attributeInfo[1] + " >" + title + "</h1>";
+                var attributeInfo = attribute.split('='); 
+                var returnString = "<h" + levelNum + " data-" + attributeInfo[0] + "=" + attributeInfo[1] + ">" + title + "</h" + levelNum + ">";
 
                 console.log(returnString);
-
                 return returnString;
             });
-
-
-
             return tmpText;
         });
     };
